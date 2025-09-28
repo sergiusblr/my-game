b15eee960908f19716a403f9f40bf0eb24e9f637
sergiusblr <sergiusblr@gmail.com>
2025-09-28 20:28:08 +0300

    11

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759067778|14468563449';
+const CACHE_VERSION = '1759080482|27173381617';
 /** @type {string} */
 const CACHE_PREFIX = 'ten-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
